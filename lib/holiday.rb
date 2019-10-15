7834d25028eb9d3f1f6f7eeed600e0c34648947e
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-15 15:33:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
       holiday.each{|holiday,supplies|
         supplies.each{|supply|
         if supply=="BBQ"
-          bbq_holidays<< holiday
+          bbq_holidays<< (holiday)
         end
         }
       }
